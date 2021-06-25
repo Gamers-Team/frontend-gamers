e2e8ec3e55c3f7a120799f05b534bd26fbf42be1
anas <civil.anas1000@gmail.com>
2021-06-25 11:08:36 +0300

    create inital components

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Test from './Test';
-import './Main.css'
-export class Main extends Component {
-    render() {
-        return (
-            <div className="main_body">
-                <Router>
-              <Switch>
-                <Route exact path="/features">
-                <Test/>
+import Test from "./Test";
+import "./Main.css";
+import Community from "./Pages/Community";
+import News from "./Pages/News";
+import Store from "./Pages/Store";
 
-                </Route>
-              </Switch>
-              </Router>
-            </div>
-        )
-    }
+export class Main extends Component {
+  render() {
+    return (
+      <div className="main_body">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Test />
+            </Route>
+
+            <Route path="/store">
+              <Store />
+            </Route>
+
+            <Route path="/news">
+              <News />
+            </Route>
+
+            <Route path="/community">
+              <Community />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
