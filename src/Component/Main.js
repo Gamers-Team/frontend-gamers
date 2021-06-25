e15f4ee3c3582d9b8fec4f875327afdf54008758
anas <civil.anas1000@gmail.com>
2021-06-25 10:33:07 +0300

    adding Routs

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './Main.css'
 export class Main extends Component {
     render() {
         return (
             <div className="main_body">
                 
-              main
+              <switch>
+              <Route exact path="/">    
+          <Route/>
+              </switch>
 
             </div>
         )
