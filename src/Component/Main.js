e405b9d02243278513c97eb41b22c06e3e5e8ad3
anas <civil.anas1000@gmail.com>
2021-06-25 11:01:06 +0300

    finish nav bar naming

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Test from './Test';
 import './Main.css'
 export class Main extends Component {
     render() {
         return (
             <div className="main_body">
-                
-              <switch>
-              <Route exact path="/">    
-          <Route/>
-              </switch>
+                <Router>
+              <Switch>
+                <Route exact path="/features">
+                <Test/>
 
+                </Route>
+              </Switch>
+              </Router>
             </div>
         )
     }
--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, Navbar, NavDropdown,Nav} from 'react-bootstrap';
+import './NavBar.css'
 
 
 export class NavBar extends Component {
@@ -8,31 +9,32 @@ export class NavBar extends Component {
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">Gamers </Navbar.Brand>
+            <Navbar.Brand href="/">Gamers </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
+
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/store">Store</Nav.Link>
+                <Nav.Link href="/news">News</Nav.Link>
+                <Nav.Link href="/community"> Our Community</Nav.Link>
+
+              </Nav>
+              <Nav className='other_side'>
+                <button> log in</button>
+                  <NavDropdown title="UserNameis here " id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                  <NavDropdown.Item href="/wishlist">
+                    My Wish List 
                   </NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.3">
                     Something
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">
-                    Separated link
+                    log out 
                   </NavDropdown.Item>
                 </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
