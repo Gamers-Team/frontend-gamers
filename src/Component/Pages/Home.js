0cb9de8ae85c4f8e00b99e57f2e86a16643840d5
mohammadhayajneh2014 <https://github.com/mohammadhayajneh2014>
2021-06-26 18:10:49 +0300

    home

--- src/Component/Pages/Home.js
+++ src/Component/Pages/Home.js
@@ -1,61 +1,104 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, CardColumns } from 'react-bootstrap';
+
 
 
 export class Home extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            gameData: [],
-            err:" "
+            game: [],
+            err: " "
 
         }
     }
 
-    componentDidMount() {
-        this.gitinggame()
-    }
-    // `https://www.gamerpower.com/api/giveaways`
-    gitinggame = async () => {
-        let server = process.env.React_APP_SERVER;
-        let homeUrl = `${server}/Home`;
-     
+    componentDidMount = async () => {
 
+        // let server = process.env.React_APP_SERVER;
+        let homeUrl = `https://api.rawg.io/api/games?key=995491d1a18f47709f7ba3b66bce85c1`;
         axios
-        .get(homeUrl)
-        .then((data=>{
-            this.setState({data:data.data})
-        }))
-        .catch((err)=>{
-            this.setState({err:there is no games})
-        }) 
-               console.log(this.state.data);
+            .get(homeUrl)
+            .then((result => {
+                let gamedata = result.data.results;
+                this.setState({
+                    game: gamedata
+
+                })
+
+            }))
+        // .catch((err) => {
+        //     this.setState({ err: 'there is no games' })
+        // })
 
     }
     render() {
         return (
             <>
-                {this.state.gameData.map((item) => {
-                    return (
-                        <div>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={item.image} />
+                {console.log(this.state.game)}
+                { for (let index = 0; index < array.length; index++) {
+                 
+                 return(
+
+                    <div>
+                    <h1>Home page</h1>
+                    <div className='heroDiv'>
+                        <div className='leftImage'>
+                            <Card style={{ width: '35rem', height: '20rem' }}>
+                                <Card.Img variant="top" src={item.background_image} />
                                 <Card.Body>
-                                    <Card.Title>{item.title}</Card.Title>
+                                    <Card.Title></Card.Title>
                                     <Card.Text>
-                                        {item.thumbnail}
+
                                     </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
                                 </Card.Body>
                             </Card>
-
-
                         </div>
-                    )
-                })
+                        <div className='rightColumn' style={{ width: '10rem', height: '20rem' }}>
+                            <CardColumns>
+                                <Card className='cardcss' >
+                                    <Card.Img variant="top" src="" />
+                                    <Card.Body>
+                                        <Card.Title>1 </Card.Title>
+                                        <Card.Text>
+
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+
+                            </CardColumns></div>
+                    </div>
+                    <div className='trending'>
+
+                    </div>
+
+                </div>
+                  )
+                  }
                 }
+                <div>
+                    {this.state.game.map((item) => {
+                        return (
+                            <div>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={item.background_image} />
+                                    <Card.Body>
+                                        <Card.Title>{item.name}</Card.Title>
+                                        <Card.Text>
+                                            {/* {item.thumbnail} */}
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+
+
+                            </div>
+                        )
+                    })
+                    }
+                </div>
             </>
         )
     }
