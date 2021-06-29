0de83fda1a56770a85844cd75b587d21a75def10
abdu-zeyad <abd.d@live.com>
2021-06-29 19:41:39 +0300

    A

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,126 +1,112 @@
 import React, { Component } from "react";
 import { Card } from "react-bootstrap";
 import "./Footer.css";
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container } from "react-bootstrap";
 
-import { FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa';
-
+import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 
 export class Footer extends Component {
   render() {
     return (
-    // <div className='bigdiv'>
-    //   <div className='smaalDiv'>
+      // <div className='bigdiv'>
+      //   <div className='smaalDiv'>
 
+      //     <div className='foterNavbar'>
+      //       <ol>
+      //         <li>
+      //           <p className='Resources' >Resources</p>
+      //         </li>
+      //         <li>
+      //           <a href="/store" className='link'>Store</a>
+      //         </li>
+      //         <a href="/news" className='link'>News</a>
+      //         <li>
+      //           <a href="/aboutUs" className='link'>About us</a>
+      //         </li>
+      //       </ol>
+      //     </div>
 
-    //     <div className='foterNavbar'>
-    //       <ol>
-    //         <li>
-    //           <p className='Resources' >Resources</p>
-    //         </li>
-    //         <li>
-    //           <a href="/store" className='link'>Store</a>
-    //         </li>
-    //         <a href="/news" className='link'>News</a>
-    //         <li>
-    //           <a href="/aboutUs" className='link'>About us</a>
-    //         </li>
-    //       </ol>
-    //     </div>
+      //     <div className='copy'>
+      //       <p className='copuright' >
+      //         &copy;2021,  Gamers, Inc. All rights reserved.Gamers , Gamers Games, the Gamers Games logo, Inc. in the Jordan and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Gamers Games International.
+      //       </p>
 
-    //     <div className='copy'>
-    //       <p className='copuright' >
-    //         &copy;2021,  Gamers, Inc. All rights reserved.Gamers , Gamers Games, the Gamers Games logo, Inc. in the Jordan and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Gamers Games International.
-    //       </p>
+      //     </div>
 
-    //     </div>
+      //     <div className='social' style={{ width: '80%' }}>
+      //       <FaFacebook className='anas' />
+      //       <FaTwitter className='anas' />
+      //       <FaYoutube className='anas' />
+      //     </div>
 
-    //     <div className='social' style={{ width: '80%' }}>
-    //       <FaFacebook className='anas' />
-    //       <FaTwitter className='anas' />
-    //       <FaYoutube className='anas' />
-    //     </div>
+      //   </div>
+      //   </div>
 
+      <div>
+        {/* <section></section> */}
+        <footer class="footer-distributed">
+          <div class="footer-left">
+            <h3>
+              Space<span>Games</span>
+            </h3>
 
-    //   </div>
-    //   </div>
+            <p class="footer-links">
+              <a href="./" class="link-1">
+                Home
+              </a>
 
+              <a href="/store">Store</a>
 
-    
-<div>
-<section>Footer Example 4</section>
-<footer class="footer-distributed">
+              <a href="/news">News</a>
 
-			<div class="footer-left">
+              <a href="/community">Our Community</a>
 
-				<h3>Company<span>logo</span></h3>
+              <a href="/aboutUs">About us</a>
+            </p>
 
-				<p class="footer-links">
-					<a href="#" class="link-1">Home</a>
-					
-					<a href="#">Blog</a>
-				
-					<a href="#">Pricing</a>
-				
-					<a href="#">About</a>
-					
-					<a href="#">Faq</a>
-					
-					<a href="#">Contact</a>
-				</p>
+            <p class="footer-company-name">SpaceGames © 2021</p>
+          </div>
 
-				<p class="footer-company-name">Company Name © 2015</p>
-			</div>
+          <div class="footer-center">
+            <div>
+              <i class="fa fa-map-marker"></i>
+              <p>
+                <span></span> Amman, Jordan
+              </p>
+            </div>
 
-			<div class="footer-center">
+            <div>
+              <i class="fa fa-phone"></i>
+              <p>+962 777777777</p>
+            </div>
 
-				<div>
-					<i class="fa fa-map-marker"></i>
-					<p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
-				</div>
+            <div>
+              <i class="fa fa-envelope"></i>
+              <p>
+                <a href="mailto:support@company.com">spacegames@company.com</a>
+              </p>
+            </div>
+          </div>
 
-				<div>
-					<i class="fa fa-phone"></i>
-					<p>+1.555.555.5555</p>
-				</div>
+          <div class="footer-right">
+            <p class="footer-company-about">
+              <span></span>
+              2021, Gamers, Inc. All rights reserved.Gamers , Gamers Games, the
+              Gamers Games logo, Inc. in the Jordan and elsewhere. Other brands
+              or product names are the trademarks of their respective owners.
+              Non-US transactions through Gamers Games International.
+            </p>
 
-				<div>
-					<i class="fa fa-envelope"></i>
-					<p><a href="mailto:support@company.com">support@company.com</a></p>
-				</div>
-
-			</div>
-
-			<div class="footer-right">
-
-				<p class="footer-company-about">
-					<span>About the company</span>
-					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-				</p>
-
-				<div class="footer-icons">
-
-					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-github"></i></a>
-
-				</div>
-
-			</div>
-
-		</footer>
-
-
-	
-
+            <div class="footer-icons">
+              <FaFacebook className="anas" />
+              <FaTwitter className="anas" />
+              <FaYoutube className="anas" />
+            </div>
+          </div>
+        </footer>
       </div>
-
     );
   }
 }
 
 export default Footer;
-
-
-
--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,50 +1,54 @@
 import React, { Component } from "react";
-import { Container, Navbar, NavDropdown,Nav} from 'react-bootstrap';
-import { withAuth0 } from '@auth0/auth0-react';
+import { Container, Navbar, NavDropdown, Nav } from "react-bootstrap";
+import { withAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./user/LoginButton";
 import LogoutButton from "./user/LogoutButton";
-import './NavBar.css'
-
+import "./NavBar.css";
 
 export class NavBar extends Component {
-  
   render() {
-    
     const { user, isAuthenticated } = this.props.auth0;
 
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="/">Gamers </Navbar.Brand>
+            <Navbar.Brand href="/">
+              {" "}
+              <img
+                className="imagelogo"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTAG43vfSQrohK7kLaGgoPyCRrCZYQPM5T5g&usqp=CAU"
+                alt="logo"
+              />{" "}
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/store">Store</Nav.Link>
                 <Nav.Link href="/news">News</Nav.Link>
                 <Nav.Link href="/community"> Our Community</Nav.Link>
                 <Nav.Link href="/aboutUs"> About us</Nav.Link>
               </Nav>
-                  <Nav className='other_side'>
-                    { isAuthenticated &&
-                  <NavDropdown title= {`${user.name}`}  id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
-                  <NavDropdown.Item href="/wishlist">
-                    My Wish List 
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="/logout">
-                    log out 
-                  </NavDropdown.Item>
-                </NavDropdown>}
+              <Nav className="other_side">
+                {isAuthenticated && (
+                  <NavDropdown
+                    title={`${user.name}`}
+                    id="collasible-nav-dropdown"
+                  >
+                    <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="/wishlist">
+                      My Wish List
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/logout">log out</NavDropdown.Item>
+                  </NavDropdown>
+                )}
 
-                { isAuthenticated ? " ": <LoginButton/>}
-
+                {isAuthenticated ? " " : <LoginButton />}
               </Nav>
             </Navbar.Collapse>
           </Container>
